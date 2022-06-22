4a888e0dd630e14c572b37aae1ab908b1b662935
ijinyu1113 <68576111+ijinyu1113@users.noreply.github.com>
2022-06-23 07:33:08 +0900

    Add files via upload

--- ui_streamlit.py
+++ ui_streamlit.py
@@ -16,7 +16,8 @@ with col1:
     # By default, uploaded files are limited to 200MB
     # You can configure this using the server.maxUploadSize config option
     uploaded_file = st.file_uploader("Supported file formats: DICOM, JPG, PNG")
-    st.download_button('Sample image', file_name='web.png')
+    with opoen('web.png', "rb") as f:
+        st.download_button('Sample image', f, file_name='web.png')
 
 if uploaded_file is not None:
     file_ext = uploaded_file.name.split(".")[-1]
